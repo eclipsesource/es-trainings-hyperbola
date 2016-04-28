a9364a0fc77cffe64390d269410233eabf1a40a7
Florian Waibel <fwaibel@eclipsesource.com>
2016-04-28 15:35:11 +0200

    Adds 1.1 Applications

---
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationActionBarAdvisor.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Jean-Michel Lemieux, Jeff McAffer and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Hyperbola is an RCP application developed for the book
+ *     Eclipse Rich Client Platform - 
+ *         Designing, Coding, and Packaging Java Applications
+ * See http://eclipsercp.org
+ *
+ * Contributors:
+ *     Jean-Michel Lemieux and Jeff McAffer - initial API and implementation
+ *******************************************************************************/
+package org.eclipsercp.hyperbola;
+
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
+
+public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+		super(configurer);
+	}
+
+	protected void makeActions(IWorkbenchWindow window) {
+	}
+
+	protected void fillMenuBar(IMenuManager menuBar) {
+	}
+
+}
