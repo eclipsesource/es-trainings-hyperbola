c15e4909770bdce4b2cd61c087466ba3dadea6ea
Florian Waibel <fwaibel@eclipsesource.com>
2016-04-28 18:23:12 +0200

    Adds 12.1 OSGi

---
+++ org.eclipsercp.hyperbola.core/src/org/eclipsercp/hyperbola/core/Activator.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2005 Jean-Michel Lemieux, Jeff McAffer and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Hyperbola is an RCP application developed for the book
+ *     Eclipse Rich Client Platform - 
+ *         Designing, Coding, and Packaging Java Applications
+ * See http://eclipsercp.org
+ *
+ * Contributors:
+ *     Jean-Michel Lemieux and Jeff McAffer - initial API and implementation
+ *******************************************************************************/
+package org.eclipsercp.hyperbola.core;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	public void start(BundleContext bundleContext) throws Exception {
+		// System.out.println("Activator.start()");
+		Activator.context = bundleContext;
+	}
+
+	public void stop(BundleContext bundleContext) throws Exception {
+		// System.out.println("Activator.stop()");
+		Activator.context = null;
+	}
+
+}
